57866b8f4a43a181bd15336a034e84fb56b5684a
Roman Zanko <roman.zanko.vl@gmail.com>
2025-05-10 16:45:43 +0400

    Handle multiple requests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,13 +7,26 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write_all(b"+PONG\r\n").unwrap();
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buffer).unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
