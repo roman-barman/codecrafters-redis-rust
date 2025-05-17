8a4c66764383401c9c3c6e0be4603e8c4d1d93b8
Roman Zanko <roman.zanko.vl@gmail.com>
2025-05-17 11:20:11 +0400

    Add ThreadPool implementation and integrate with main server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use crate::thread_pool::ThreadPool;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+
+mod thread_pool;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_client(stream);
+                pool.execute(|| {
+                    handle_client(stream);
+                })
             }
             Err(e) => {
                 println!("error: {}", e);
