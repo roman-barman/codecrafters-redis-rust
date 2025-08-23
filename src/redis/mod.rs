1e6a1b5648c96682f5969caede13464b3d21aa83
Roman Zanko <roman.zanko.vl@gmail.com>
2025-08-23 23:13:25 +0400

    Refactor: add `Storage` module with TTL support for key-value storage, update `mod.rs` to include `storage`.

--- src/redis/mod.rs
+++ src/redis/mod.rs
@@ -1,5 +1,6 @@
 mod server;
 mod message_reader;
 mod message_writer;
+mod storage;
 
 pub use server::Server;
---
+++ src/redis/storage.rs
@@ -0,0 +1,52 @@
+use chrono::{DateTime, Local, TimeDelta};
+use std::collections::HashMap;
+
+pub struct Storage {
+    storage: HashMap<String, (String, KeySettings)>,
+}
+
+impl Storage {
+    pub fn new() -> Self {
+        Self {
+            storage: HashMap::new()
+        }
+    }
+
+    pub fn get(&mut self, key: &str) -> Option<&str> {
+        let should_remove = match self.storage.get(key) {
+            None => return None,
+            Some((_, settings)) => settings.ttl.is_some() && settings.ttl.unwrap() <= Local::now(),
+        };
+
+        if should_remove {
+            self.storage.remove(key);
+            None
+        } else {
+            self.storage.get(key).map(|(v, _)| v.as_ref())
+        }
+    }
+
+    pub fn set(&mut self, key: String, value: String, key_settings: KeySettings) {
+        self.storage.insert(key, (value, key_settings));
+    }
+}
+
+pub struct KeySettings {
+    ttl: Option<DateTime<Local>>,
+}
+
+impl Default for KeySettings {
+    fn default() -> Self {
+        Self {
+            ttl: None
+        }
+    }
+}
+
+impl KeySettings {
+    pub fn new(expiry: u64) -> Self {
+        Self {
+            ttl: Some(Local::now() + TimeDelta::try_milliseconds(expiry as i64).unwrap())
+        }
+    }
+}
