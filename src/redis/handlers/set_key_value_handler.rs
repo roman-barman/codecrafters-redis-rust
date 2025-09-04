a9f7598a4afac6a3bae6b7a8a9741205f7aced72
Roman Zanko <roman.zanko.vl@gmail.com>
2025-09-04 22:28:57 +0400

    Refactor: simplify `set_key_value` logic, improve error handling, and streamline `KeySettings` initialization.

--- src/redis/handlers/set_key_value_handler.rs
+++ src/redis/handlers/set_key_value_handler.rs
@@ -8,35 +8,33 @@ use thiserror::Error;
 pub trait SetKeyValueHandler: GetStorage {
     fn set_key_value(&mut self, request: &Request) -> Result<Response, SetKeyValueHandlerError> {
         if request.len() < 3 {
-            Err(SetKeyValueHandlerError::WrongNumberOfArguments)
-        } else {
-            let key = request.get(1).unwrap().to_string();
-            let value = request.get(2).unwrap().to_string();
-            let settings = match request.get(3) {
-                None => Ok(KeySettings::default()),
-                Some(value) => {
-                    let arg_name = value.to_lowercase();
-                    if "px" != arg_name {
-                        Err(SetKeyValueHandlerError::UnknownArgument(value.to_string()))
-                    } else {
-                        let arg_value = request.get(4);
-                        if arg_value.is_none() {
-                            Err(SetKeyValueHandlerError::WrongNumberOfArguments)
-                        } else {
-                            let ttl = arg_value.unwrap();
-                            match ttl.parse::<u64>() {
-                                Ok(ttl) => Ok(KeySettings::new(ttl)),
-                                Err(_) => {
-                                    Err(SetKeyValueHandlerError::InvalidPxValue(ttl.to_string()))
-                                }
-                            }
-                        }
-                    }
-                }
-            }?;
-            self.get_storage().set(key, value, settings);
-            Ok(Response::BulkString(Some("OK".to_string())))
+            return Err(SetKeyValueHandlerError::WrongNumberOfArguments);
         }
+
+        let key = request.get(1).unwrap().to_string();
+        let value = request.get(2).unwrap().to_string();
+        let settings = match request.get(3) {
+            None => KeySettings::default(),
+            Some(value) => {
+                let arg_name = value.to_lowercase();
+                if "px" != arg_name {
+                    return Err(SetKeyValueHandlerError::UnknownArgument(value.to_string()));
+                }
+
+                let arg_value = request.get(4);
+                if arg_value.is_none() {
+                    return Err(SetKeyValueHandlerError::WrongNumberOfArguments);
+                }
+
+                let ttl = arg_value.unwrap();
+                match ttl.parse::<u64>() {
+                    Ok(ttl) => KeySettings::new(ttl),
+                    Err(_) => return Err(SetKeyValueHandlerError::InvalidPxValue(ttl.to_string()))
+                }
+            }
+        };
+        self.get_storage().set(key, value, settings);
+        Ok(Response::BulkString(Some("OK".to_string())))
     }
 }
 
