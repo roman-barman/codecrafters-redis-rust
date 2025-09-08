944eb568a44fec317aa37d7cbadf3ea50752cb16
Roman Zanko <roman.zanko.vl@gmail.com>
2025-09-08 11:08:34 +0400

    Implement generic `read_length` function for length decoding, add `InvalidLengthEncoding` error, and include unit tests for `read_length`.

--- src/redis/rdb/database_reader.rs
+++ src/redis/rdb/database_reader.rs
@@ -41,22 +41,33 @@ where
     Ok(std::str::from_utf8(&version)?.to_string())
 }
 
-/*fn read_length(file: &mut File) -> Result<u64, DatabaseReaderError> {
+fn read_length<T>(file: &mut T) -> Result<u32, DatabaseReaderError>
+where
+    T: Read,
+{
     let mut length = [0u8; 1];
     file.read_exact(&mut length)?;
 
-    let flag = length[0] & 0xc0;
+    let flag = length[0] >> 6;
 
     if flag == 0 {
-        return Ok((length[0] & 0x3f) as u64);
+        return Ok((length[0] & 0x3f) as u32);
     }
 
-    if flag == 0x40 {
-        let mut length = [0u8; 1];
-        file.read_exact(&mut length)?;
-        return Ok(((length[0] as u64) << 8) | (length[1] as u64));
+    if flag == 1 {
+        let mut length_2 = [0u8; 1];
+        file.read_exact(&mut length_2)?;
+        return Ok(u32::from_be_bytes([0, 0, length[0] & 0x3f, length_2[0]]));
     }
-}*/
+
+    if flag == 2 {
+        let mut length = [0u8; 4];
+        file.read_exact(&mut length)?;
+        return Ok(u32::from_be_bytes(length));
+    }
+
+    Err(DatabaseReaderError::InvalidLengthEncoding)
+}
 
 #[derive(Debug, Error)]
 pub enum DatabaseReaderError {
@@ -64,11 +75,13 @@ pub enum DatabaseReaderError {
     Io(#[from] std::io::Error),
     #[error("UTF8 error")]
     Utf8(#[from] std::str::Utf8Error),
+    #[error("invalid length encoding")]
+    InvalidLengthEncoding,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::redis::rdb::database_reader::{is_rdb_file, read_rdb_version};
+    use crate::redis::rdb::database_reader::{is_rdb_file, read_length, read_rdb_version};
     use std::io;
 
     #[test]
@@ -86,5 +99,26 @@ mod tests {
         assert_eq!(
             read_rdb_version(&mut io::Cursor::new(b"REDIS0001")).unwrap(),
             "0001".to_string());
+    }
+
+    #[test]
+    fn test_read_length_6_bits() {
+        assert_eq!(
+            read_length(&mut io::Cursor::new([0x2a])).unwrap(),
+            42);
+    }
+
+    #[test]
+    fn test_read_length_14_bits() {
+        assert_eq!(
+            read_length(&mut io::Cursor::new([0x6a, 0xaa])).unwrap(),
+            10922);
+    }
+
+    #[test]
+    fn test_read_length_32_bits() {
+        assert_eq!(
+            read_length(&mut io::Cursor::new([0x80, 0xff, 0x00, 0xff, 0x00])).unwrap(),
+            4278255360);
     }
 }
